24564d6801fff56e6cbc28d28f07d4adad3d8748
Eray Demircioglu <75967993+eraydmrcoglu@users.noreply.github.com>
2024-04-02 16:18:30 +0300

    Reddit

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -34,6 +34,6 @@ export async function GET(req: NextRequest) {
   return NextResponse.redirect(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/"
-      : "https://nextjs-reddit-seven.vercel.app"
+      : "https://nextjs-reddit-seven.vercel.app/"
   );
 }
